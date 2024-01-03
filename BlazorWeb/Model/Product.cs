37787328433f4de94b6107fc2e33bccd28c2c5e7
Jonathan Ndambuki <joendambuki16@gmail.com>
2024-01-03 11:38:59 +0300

    Add project files.

---
+++ BlazorWeb/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorWeb.Model
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }=string.Empty;
+
+        public string Description { get; set; }= string.Empty;
+
+        public int Price { get; set; }
+
+        public string Image { get; set; } = string.Empty;
+    }
+}
